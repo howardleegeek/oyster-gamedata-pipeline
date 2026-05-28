a335d30ca7ca1e74fcf7b2966f317da983e2687f
Howard Li <howard.linra@gmail.com>
2026-05-27 23:57:28 -0700

    test(recorder): avoid audio fallback in game-state package test

--- tests/test_recorder_lite_rc9_ux.py
+++ tests/test_recorder_lite_rc9_ux.py
@@ -321,6 +321,7 @@ def test_package_preserves_raw_game_state_jsonl_and_transforms_action_camera(
     }
     app._captured_events = []
     app._session_id = "unit-session"
+    app._audio_probe_failed = False
     setattr(app, "_allow_" + "place" + "holder", False)
 
     tar_path = app._package_tarball("20260527-000000")
